3d548702b602b02e80e9b89aaf2fad683fc76d78
Ivan <shunyaeviv@63pokupki.ru>
2021-01-12 10:48:21 +0400

    Add

---
+++ Vknob.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="wrap-vknob">
+    <label for="range-vknob">{{ lable }}</label>
+    <b-form-input
+      id="range-vknob"
+      v-model="knobValue"
+      type="range"
+      @change="changeValue"
+      min="min"
+      max="max"
+      step="step"    
+    >
+    </b-form-input>
+    <img :src="imgURL.knobSplitterUrl" alt="splitter">
+    <img :src="imgURL.knobControlUrl" alt="control">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Vknob",
+
+  props: {
+    knobValue: {
+      type: Number,
+      required: true,
+    },
+    // Метод управляющий значением
+    changeValue: {
+      type: Function,
+      required: true
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    },
+    // Шаги увеличения значения
+    step: {
+      type: Number,
+      default: 0.5,
+    },
+    // Возможность деактивировать на некоторых инструментах или например в бесплатной версии
+    disable: {
+      type: Boolean,
+      default: false
+    },
+    // НАзвание регулятора
+    lable: {
+      type: String,
+      required: true,
+    },
+    imgURL: {
+      // Ссылка на изображение регулятора
+      knobControlUrl: String,
+      // Ссылка на изображение делителей(указателей уровня)
+      knobSplitterUrl: String,
+    },
+
+  },
+};
+</script>
+
+<style></style>
+
+<style scoped>
+  /* TODO  */
+  /* 1. В темплейте в разметку добавляем стандартный range или его аналоги. В данном
+  случае из bootstrap-vue компонент b-form-input 
+  2. Прописываем пропсы  
+  3. name: 'Vknob', - задаём имя
+  компоненту 
+  4. Прописываем все аргументы, события и директивы: 
+  - id="range-vknob" для связи с лейблом; 
+  - v-model="knobValue" с переданным пропсом knobValue 
+  - @change="changeValue" через событие change управляем регулятором 
+  - :min="min" :max="max" - минимальное и максимальное значение, значения передаём из пропсов
+  - :step="step" - шаг, также подтягиваем из пропсов
+  - disable - для включения/выключения регулятора
+  - lable - название регулятора. Может быть также, например звук, хор, тональность и т.д.
+  
+  5. Добавляем в разметку изображения регулятора и в пути указываем пропсы, например регулятора imgURL.knobControlUrl 
+
+  6. Используем компонент внутри родительского:
+  - прописываем внутри компонента атрибуты пропсов
+  - передаём в них необходимые значения
+  - Пишем метод, управляющий регулятором и передаём его в changeValue
+  - Передаём ссылки на изображения
+  
+   */
+</style>
+ 
